9da690221e01a73c4a3d031494d050335028a1da
Khainguyen912 <nguyenthanhkhai199x@gmail.com>
2024-03-13 20:05:44 -0700

    add item list layout

--- app/src/main/java/com/example/g1_csis3175_002/ListItemSeller.java
+++ app/src/main/java/com/example/g1_csis3175_002/ListItemSeller.java
@@ -8,17 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ListItemFromSeller extends AppCompatActivity {
+public class ListItemSeller extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_list_item_from_seller);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_list_item_seller);
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        }); */
+
+
+
     }
 }

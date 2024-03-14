68646b8663c8e5e17a34fa7f6ccde6ea2a3fb723
Raymond <raymond.wang68@outlook.com>
2024-03-14 12:43:15 +0800

    first commit after Munique merged all of our files

---
+++ app/src/main/java/com/example/g1_csis3175_002/ProductGVAdapter.java
@@ -0,0 +1,37 @@
+package com.example.g1_csis3175_002;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class ProductGVAdapter extends ArrayAdapter<ProductModel> {
+    public ProductGVAdapter(@NonNull Context context, ArrayList<ProductModel> productModelArrayList) {
+        super(context, 0, productModelArrayList);
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View listitemView = convertView;
+        if (listitemView == null){
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.listview_layout_buying, parent, false);
+        }
+        ProductModel productModel = getItem(position);
+        TextView productName = listitemView.findViewById(R.id.tvProductName);
+        TextView productPrice = listitemView.findViewById(R.id.tvPrice);
+        ImageView productImage = listitemView.findViewById(R.id.imgProduct);
+        productName.setText(productModel.getProductName());
+        productImage.setImageResource((productModel.getImgid()));
+        productPrice.setText(String.format(Locale.US, "$%.2f", productModel.getPrice()));
+        return listitemView;
+    }
+}
